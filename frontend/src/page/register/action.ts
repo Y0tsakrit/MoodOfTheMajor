2b9893ae7684bc20b8a71448892a9a1201a7635f
Yotsakrit Reuangkaeo <67011392@kmitl.ac.th>
2026-07-10 22:28:42 +0700

    change axios to api

--- frontend/src/page/register/action.ts
+++ frontend/src/page/register/action.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import api from "../../utils/api";
 import type { RegisterDTO } from "../../interface/registerDTO";
 
 export const getFaculties = async () => {
     try{
-        const response = await axios.get(`${import.meta.env.VITE_URL_API}/department/search`);
+        const response = await api.get(`${import.meta.env.VITE_URL_API}/department/search`, { withCredentials: false });
         let faculties = response.data.map((faculty: any) => ({
             value: faculty.faculty,
             label: faculty.faculty,
@@ -18,7 +18,7 @@ export const getFaculties = async () => {
 
 export const getMajors = async (faculty: string) => {
     try{
-        const response = await axios.get(`${import.meta.env.VITE_URL_API}/department/search?faculty=${faculty}`);
+        const response = await api.get(`${import.meta.env.VITE_URL_API}/department/search?faculty=${faculty}`, { withCredentials: false });
         let majors = response.data.map((major: any) => ({
             value: major.major,
             label: major.major,
@@ -42,7 +42,7 @@ export const registerUser = async (data: RegisterDTO) => {
         }
     }
     try {
-        const response = await axios.post(`${import.meta.env.VITE_URL_API}/auth/register`, payload);
+        const response = await api.post(`${import.meta.env.VITE_URL_API}/auth/register`, payload, { withCredentials: false });
         return response.data;
     } catch (error) {
         throw error;
